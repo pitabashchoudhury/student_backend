a049f06b404b34954f2ffe9b4a6937e526155ee8
pitabashchoudhury <pitabash98@gmail.com>
2021-02-19 10:50:31 +0530

    resource applicarion changed

--- src/main/java/com/student/config/Swagger2config.java
+++ src/main/java/com/student/config/Swagger2config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -20,11 +21,11 @@ public class Swagger2config {
     @Bean
     public Docket Api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.student")).build();
+                .apis(RequestHandlerSelectors.basePackage("com.student")).paths(PathSelectors.any()).build().apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("student Web Service").description("Backend for product Website/App")
+        return new ApiInfoBuilder().title("student Web Service").description("Backend for student Website/App")
                 .contact(new Contact("pitabash", "", "pitabash98@gmail.com")).version(version).build();
     }
 
